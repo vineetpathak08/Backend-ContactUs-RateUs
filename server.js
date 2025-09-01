f65825116ed85bf78b91fddeb7804195e2d88dfe
Vineet Pathak <pathakvineet2005@gmail.com>
2025-09-02 03:21:56 +0530

    server.js Indentation correction

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 // Rate Limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 100, 
 });
 app.use(limiter);
 
